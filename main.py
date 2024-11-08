897826431fd1c07ae5367c40545e7c6c0bb6f073
lmunier <lmunier@protonmail.com>
2024-11-08 21:50:32 +0100

    [Feat] First working process. Shuffling list of people based on criteria such as not redundant over the years and accept to have unwanted pairs.

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ avoid sending mail to the same person each time.
 """
 
 import os
+import sys
 import re
 import getpass
 import random
@@ -26,6 +27,8 @@ from io import StringIO
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from datetime import datetime
+
+from itertools import permutations
 from ruamel.yaml import YAML
 
 
@@ -40,7 +43,7 @@ def main():
     global_config = get_global_config()
     private_config = get_config(global_config["private_folder"])
 
-    # Recover peoples from file
+    # Get peoples from file
     config_sublist = input("[Question] - Which yaml sublist to take ? [test_config] ")
     if config_sublist == "":
         config_sublist = "test_config"
@@ -48,12 +51,12 @@ def main():
         print("[ERROR] - This sublist does not exist.")
         return
 
-    list_people = recover_people(global_config["private_folder"], config_sublist)
-
-    # Shuffle list if
-    answer = input("[Question] - Would you like to shuffle list ? [y/N] ")
-    if answer.lower() == "y":
-        random.shuffle(list_people)
+    # Get the list of people shuffled
+    list_people = get_santas_list(
+        global_config["private_folder"],
+        config_sublist,
+        private_config["year_before_repeat"],
+    )
 
     # Save people order
     output_file = os.path.join(
@@ -109,15 +112,19 @@ def get_config(private_folder: str) -> dict:
     return config
 
 
-def recover_people(
-    private_folder: str, config_sublist: str, year: str = datetime.now().year
-) -> list:
+def get_people(
+    private_folder: str,
+    people_list: str,
+    config_sublist: str,
+    year: str = datetime.now().year,
+) -> tuple:
     """
-    Recover people from a given input file, return it as a list of tuples.
+    Get people from a given input file, return it as a list of tuples.
 
     Args:
         private_folder (str): The path to the private folder containing the input
                               and output files.
+        people_list (str): The name of the file containing the list of people.
         config_sublist (str): The sublist to take configuration from.
         year (str): The year to use for the sublist.
 
@@ -127,22 +134,157 @@ def recover_people(
     """
     # Get the input file to use
     input_sublist = ""
-    input_file = os.path.join(private_folder, config_sublist, "input_mail_list.yaml")
+    input_file = os.path.join(private_folder, config_sublist, people_list)
 
     if config_sublist == "test_config":
         input_sublist = "test_config"
     else:
         input_sublist = f"year_{year}"
 
-    # Recover peoples from file
+    # Get peoples from file
     with open(input_file, "r", encoding="utf-8") as info:
         dict_info_people = yaml.load(info)[input_sublist]
 
     if not dict_info_people:
         print("[ERROR] - List of people is empty.")
-        return []
+        return [], []
 
-    return list(dict_info_people.items())
+    if "unwanted" in dict_info_people:
+        # Split the list of people into wanted and unwanted people
+        unwanted_people = list(dict_info_people["unwanted"].items())
+        dict_info_people.pop("unwanted")
+
+        return list(dict_info_people.items()), unwanted_people
+
+    return list(dict_info_people.items()), []
+
+
+def compute_all_possibilities(
+    private_folder: str, config_sublist: str, nb_years: int
+) -> list:
+    """
+    Compute possible pairs from all the possibilities and removing unwanted ones:
+    - Same as old ones
+    - Same as unwanted ones
+
+    Args:
+        list_people (list): A list of tuples, where each tuple contains the name
+                            and additional information of a person.
+
+    Returns:
+        list: A list with all the possible pairs without keeping the ones from
+              the X previous years and the unwanted ones.
+    """
+    # Extract names from the list of tuples and generate all the possible pairs
+    list_people, list_unwanted = get_people(
+        private_folder, "input_mail_list.yaml", config_sublist
+    )
+
+    names = [person[0] for person in list_people]
+    all_pairs = list(permutations(names, 2))
+
+    # Get all the pairs from X previous years
+    current_year = datetime.now().year
+    for i in range(nb_years):
+        old_list_people, _ = get_people(
+            private_folder,
+            "output_mail_list.yaml",
+            config_sublist,
+            year=current_year - i - 1,
+        )
+
+        old_pairs = [
+            (old_list_people[i][0], old_list_people[(i + 1) % len(old_list_people)][0])
+            for i, _ in enumerate(old_list_people)
+        ]
+
+        # Remove the pairs from the old list
+        all_pairs = [pair for pair in all_pairs if pair not in old_pairs]
+
+    # Remove the unwanted pairs
+    unwanted_pairs = []
+    for unwanted in list_unwanted:
+        for unwanted_target in unwanted[1]:
+            unwanted_pairs.append((unwanted[0], unwanted_target))
+
+    all_pairs = [pair for pair in all_pairs if pair not in unwanted_pairs]
+
+    return all_pairs
+
+
+def get_santas_list(private_folder: str, config_sublist: str, nb_years: int) -> list:
+    """
+    Shuffle the list of people until no one has the same assigned person as
+    for the last nb_years of yeas.
+
+    Args:
+        private_folder (str): The path to the private folder containing the input
+                              and output files.
+        config_sublist (str): The sublist to take configuration from.
+        nb_years (int): The number of years to consider when shuffling the list.
+
+    Returns:
+        list: The shuffled list of people.
+    """
+    max_iteration = 1000
+    list_people = get_people(private_folder, "input_mail_list.yaml", config_sublist)[0]
+
+    # Shuffle list if and get all possibiles pairs based on criterias from the config files
+    answer = input("[Question] - Would you like to shuffle list ? [y/N] ")
+    if not answer.lower() == "y":
+        return list_people
+
+    possible_list = compute_all_possibilities(private_folder, config_sublist, nb_years)
+
+    # Extract santas from the list of tuples
+    list_santas = []
+    random.shuffle(possible_list)
+
+    first_run = True
+    iteration = 0
+    while iteration < max_iteration:
+        while len(list_santas) < len(list_people) and possible_list:
+            if first_run:
+                santa = possible_list[0][0]
+                next_santa = possible_list[0][1]
+                first_run = False
+            else:
+                santa = next_santa
+
+                try:
+                    next_santa = next(
+                        (pair[1] for pair in possible_list if pair[0] == santa)
+                    )
+                except StopIteration:
+                    break
+
+            list_santas.append(
+                next((person for person in list_people if person[0] == santa), None)
+            )
+
+            # Remove all the pairs beginning with the current santa
+            possible_list = [pair for pair in possible_list if santa not in pair]
+
+            if len(list_santas) == len(list_people) - 1:
+                list_santas.append(
+                    next(
+                        (person for person in list_people if person[0] == next_santa),
+                        None,
+                    )
+                )
+
+        if len(list_santas) == len(list_people):
+            break
+
+        first_run = True
+        list_santas = []
+        iteration += 1
+
+    if len(list_santas) == len(list_people):
+        return list_santas
+
+    print("[ERROR] - Could not shuffle list with the current conditions.")
+    sys.exit(1)
 
 
 def save_people(list_people: list, output_file: str):
